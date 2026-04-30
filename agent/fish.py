3df60b0998a54ebb897f4ec6007ab8ad5d10ea83
op200 <112297583+op200@users.noreply.github.com>
2026-04-30 20:31:12 +0800

    v0.1.4 feat: 优化性能 feat: 增加脱钩后处理

--- agent/fish.py
+++ agent/fish.py
@@ -7,6 +7,8 @@ from maa.custom_action import CustomAction
 if TYPE_CHECKING:
     from maa.context import Context
 
+GY_ROI = [400, 40, 500, 18]
+
 
 @AgentServer.custom_action("fish_溜鱼")
 class Fish_溜鱼(CustomAction):
@@ -18,46 +20,66 @@ class Fish_溜鱼(CustomAction):
     ) -> bool:
         controller = context.tasker.controller
 
-        img = controller.post_screencap().get(wait=True)
-        yellow_reco_detail = context.run_recognition(
-            "匹配黄色",
-            img,
-            pipeline_override={
-                "匹配黄色": {
-                    "recognition": {
-                        "type": "ColorMatch",
-                        "param": {
-                            "roi": [400, 40, 500, 18],
-                            "lower": [216, 189, 80],
-                            "upper": [253, 252, 186],
-                        },
+        while True:
+            img = controller.post_screencap().get(wait=True)
+
+            greem_reco_detail = context.run_recognition(
+                "匹配绿色",
+                img,
+                pipeline_override={
+                    "匹配绿色": {
+                        "recognition": {
+                            "type": "ColorMatch",
+                            "param": {
+                                "roi": GY_ROI,
+                                "lower": [32, 178, 160],
+                                "upper": [60, 244, 197],
+                            },
+                        }
                     }
-                }
-            },
-        )
+                },
+            )
+            if greem_reco_detail is None:
+                print(f"识别绿色错误: {greem_reco_detail}")
+                return True
+            gbox = greem_reco_detail.box
+            if gbox is None:
+                # print("识别不到绿色")
+                return True
 
-        if yellow_reco_detail is None:
-            print("识别不到黄色")
-            return True
+            yellow_reco_detail = context.run_recognition(
+                "匹配黄色",
+                img,
+                pipeline_override={
+                    "匹配黄色": {
+                        "recognition": {
+                            "type": "ColorMatch",
+                            "param": {
+                                "roi": GY_ROI,
+                                "lower": [216, 189, 80],
+                                "upper": [253, 252, 186],
+                            },
+                        }
+                    }
+                },
+            )
+            if yellow_reco_detail is None:
+                print(f"识别黄色错误: {yellow_reco_detail}")
+                return True
+            ybox = yellow_reco_detail.box
+            if ybox is None:
+                # print("识别不到黄色")
+                return True
 
-        green_box = argv.box
-        yellow_box = yellow_reco_detail.box
+            green_x = gbox.x + gbox.w // 2
+            yellow_x = ybox.x + ybox.w // 2
+            diff = green_x - yellow_x
 
-        if yellow_box is None:
-            print("识别到黄色但是没有位置")
-            return True
-
-        green_x = green_box.x + green_box.w // 2
-        yellow_x = yellow_box.x + yellow_box.w // 2
-        diff = green_x - yellow_x
-
-        key = (
-            0x41  # A
-            if diff < 0
-            else 0x44  # D
-        )
-        controller.post_key_down(key).wait()
-        time.sleep(abs(diff) / 200)
-        controller.post_key_up(key).wait()
-
-        return True
+            key = (
+                0x41  # A
+                if diff < 0
+                else 0x44  # D
+            )
+            controller.post_key_down(key).wait()
+            time.sleep(min(0.6, max(0.02, abs(diff) / 200)))
+            controller.post_key_up(key).wait()
