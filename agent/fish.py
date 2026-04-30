808361887ab36a069378a59d9560817dff9d2feb
op200 <112297583+op200@users.noreply.github.com>
2026-04-30 15:47:48 +0800

    v0.1.3 钓鱼功能基本可用

--- agent/fish.py
+++ agent/fish.py
@@ -1,3 +1,4 @@
+import time
 from typing import TYPE_CHECKING, override
 
 from maa.agent.agent_server import AgentServer
@@ -15,17 +16,20 @@ class Fish_溜鱼(CustomAction):
         context: Context,
         argv: CustomAction.RunArg,
     ) -> bool:
+        controller = context.tasker.controller
+
+        img = controller.post_screencap().get(wait=True)
         yellow_reco_detail = context.run_recognition(
             "匹配黄色",
-            argv.reco_detail.raw_image,
+            img,
             pipeline_override={
                 "匹配黄色": {
                     "recognition": {
                         "type": "ColorMatch",
                         "param": {
                             "roi": [400, 40, 500, 18],
-                            "lower": [230, 213, 122],
-                            "upper": [254, 249, 170],
+                            "lower": [216, 189, 80],
+                            "upper": [253, 252, 186],
                         },
                     }
                 }
@@ -34,23 +38,26 @@ class Fish_溜鱼(CustomAction):
 
         if yellow_reco_detail is None:
             print("识别不到黄色")
-            return False
+            return True
 
         green_box = argv.box
         yellow_box = yellow_reco_detail.box
 
         if yellow_box is None:
             print("识别到黄色但是没有位置")
-            return False
+            return True
 
         green_x = green_box.x + green_box.w // 2
         yellow_x = yellow_box.x + yellow_box.w // 2
+        diff = green_x - yellow_x
 
-        click_job = context.tasker.controller.post_click_key(
+        key = (
             0x41  # A
-            if green_x < yellow_x
+            if diff < 0
             else 0x44  # D
         )
-        click_job.wait()
+        controller.post_key_down(key).wait()
+        time.sleep(abs(diff) / 200)
+        controller.post_key_up(key).wait()
 
         return True
